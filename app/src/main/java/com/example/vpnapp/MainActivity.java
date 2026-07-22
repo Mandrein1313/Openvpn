4a01a66206db4e70b4803bfab8a3c58391d00a9d
root <root@localhost>
2026-07-22 16:25:22 +0700

    Cloud Build Request - 2026-07-22 16:25:22

---
+++ app/src/main/java/com/example/vpnapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.vpnapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.vpnapp.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
